5d7980cf68663672d73bb21ba624b9e80855a487
MrSabin <sabin.21011986@gmail.com>
2023-02-17 12:45:55 +0300

    Got answer from long polling API

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import requests
 from environs import Env
 
+
 def main():
     env = Env()
     env.read_env()
     dvmn_token = env.str("DVMN_TOKEN")
 
-    url = "https://dvmn.org/api/user_reviews/"
+    url = "https://dvmn.org/api/long_polling/"
     headers = {"Authorization": dvmn_token}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    print(response.text)
+    timestamp = None
+
+    while True:
+        payload = {"timestamp": timestamp}
+        response = requests.get(
+            url, headers=headers, params=payload, timeout=120
+        )
+        response.raise_for_status()
+        server_answer = response.json()
+        print(server_answer)
+
+        if server_answer.get("timestamp_to_request"):
+            timestamp = server_answer.get("timestamp_to_request")
+        else:
+            timestamp = server_answer.get("last_attempt_timestamp")
 
 
 if __name__ == "__main__":
     main()
-    
